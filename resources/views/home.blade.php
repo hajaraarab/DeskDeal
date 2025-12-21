fc311be96b24bd62bdf33e8953803f5592ff2d02
hajaraarab <hajar.aarab@student.kdg.be>
2025-12-21 21:43:34 +0100

    Laravel basisstructuur toegevoegd

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DeskDeal</title>
+</head>
+<body>
+    <h1>dit is home</h1>
+</body>
+</html>
