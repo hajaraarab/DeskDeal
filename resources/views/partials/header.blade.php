09ee607a76b40ac7afe4fe89526b2875106454c7
hajaraarab <hajar.aarab@student.kdg.be>
2026-05-25 20:54:32 +0200

    Styling van de header toevoegen

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
 
     @vite(['resources/scss/index.scss', 'resources/js/app.js'])
 
@@ -16,10 +20,10 @@
         <h4><a href="{{ route('home') }}">DeskDeal</a></h4>
     </div>
     <ul>
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('about') }}">About</a></li>
-        <li><a href="{{ route('marketplace') }}">Marketplace</a></li>
-        <li><a href="{{ route('contact') }}">Contact</a></li>
+        <li><a href="{{ route('home') }}" class="h6 {{ request()->routeIs('home') ? 'active' : '' }}">Home</a></li>
+        <li><a href="{{ route('about') }}" class="h6 {{ request()->routeIs('about') ? 'active' : '' }}">About</a></li>
+        <li><a href="{{ route('marketplace') }}" class="h6 {{ request()->routeIs('marketplace') ? 'active' : '' }}">Marketplace</a></li>
+        <li><a href="{{ route('contact') }}" class="h6 {{ request()->routeIs('contact') ? 'active' : '' }}">Contact</a></li>
     </ul>
     <div class="user-menu">
         <p>login</p>
