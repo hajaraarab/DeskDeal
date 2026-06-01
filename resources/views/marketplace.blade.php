4cc34bbda73d017aba6bc76d5338293cbce3a154
hajaraarab <hajar.aarab@student.kdg.be>
2026-06-01 18:00:28 +0200

    Styling op de marketplace toevoegen

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -1,23 +1,42 @@
+<style>
+    body {
+        background-color: #f5f7fa;
+    }
+</style>
+
 @include('partials.header')
 
-<div class="hero-banner">
-    <h1>@yield('title')</h1>
+<div class="hero-banner-container">
+    <div class="hero-banner">
+        <h1>Marketplace</h1>
+    </div>
 </div>
 
-<div class="content">
-    <h1>Welcome to the marketplace Page</h1>
-    <p>This is the content of the marketplaces page.</p>
+<div class="content marketplace">
+
+    <div class="marketplace-text">
+        <p class="subtitle">new subtitle</p>
+        <div class="marplace-description">
+            <h2>Vind je volgende kantoorvonst</h2>
+            <p>6 items beschikbaar - bekijk per categorie</p>
+        </div>
+    </div>
 
     <div class="search-container">
         <input type="text" placeholder="Search for products...">
-        <button class="" type="submit">Search</button>
+        <button class="button-with-icon body-sm" type="submit">
+            <img src="{{ asset('images/icons/filter-icon.png') }}" alt="Filter Icon">
+            Filter
+        </button>
     </div>
 
     <div class="marketplace-container">
         <div class="sidebar">
 
-            <a href="{{ route('products.create') }}" >Voeg product toe</a>
-
+            @auth
+                <a class="round-btn darkblue body-lg" href="{{ route('products.create') }}" >+ Voeg product toe</a>
+            @endauth
+            
             <p>Categorieën</p>
 
             <a href="#" class="">Alle categorieën</a>
